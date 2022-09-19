5e59ba83087b399599bf00991043bbd1284d2432
Sreetam <sreetam.ganguly@rebelfoods.com>
2022-09-19 17:47:33 +0530

    Output formatting

--- breaking.py
+++ breaking.py
@@ -6,31 +6,32 @@ import time
 import re
 import csv
 
+from warnings import filterwarnings
+import sys
+filterwarnings("ignore")
+
 mins = 15
 articles = []
 rss_list = pd.read_csv('./rss/rss.csv')
 
 for rss in rss_list['rss-url']:
   try:
-    for i in range(2):
-      r = requests.get(rss, timeout=0.4)
-      if r.status_code==200:
-        break
+    r = requests.get(rss, timeout=0.4)
     soupContent = BeautifulSoup(r.content,features='xml')
     items = soupContent.findAll('item')
     if r.status_code==200 and len(items)!=0:
-      print('Total News Content: ' + str(len(items)) + ' : Success: ' + str(r.status_code) + " : " + rss)
+      print('\x1b[2K\rTotal News Content: ' + str(len(items)) + ' : Success: ' + str(r.status_code) + " : " + rss, end="")
     else:
-      print('Total News Content: 0 : Fail: ' + str(r.status_code) + ' : ' + rss)
+      print('\x1b[2K\rTotal News Content: 0 : Fail: ' + str(r.status_code) + ' : ' + rss, end="")
       continue
   except:
-    print('Total News Content: 0 : Fail: 404 : ' + rss)
+    print('\x1b[2K\rTotal News Content: 0 : Fail: 404 : ' + rss, end="")
     continue
   for i in items:
     article = []
     try:
-      date = re.sub("<.+?>", " ", i.find('pubDate').text).strip()
-      stamp = parse(date, fuzzy=True).timestamp()
+      date = parse(re.sub("<.+?>", " ", i.find('pubDate').text).strip(), fuzzy=True)
+      stamp = date.timestamp()
       if stamp>=time.time() - mins*60:
         article.append(date)
         article.append(stamp)
@@ -38,9 +39,19 @@ for rss in rss_list['rss-url']:
         continue
     except:
        continue
-    try: article.append(re.sub("<.+?>", " ", i.find('title').text).strip())
+    try:
+      title = i.find('title').text
+      title = re.sub("<.+?>", " ", title)
+      title = re.sub("&nbsp;", " ", title)
+      title = re.sub("[\n ]+", " ", title).strip()
+      article.append(title)
     except: continue
-    try: article.append(re.sub("<.+?>", " ", i.find('description').text).strip())
+    try:
+      description = i.find('description').text
+      description = re.sub("<.+?>", " ", description)
+      description = re.sub("&nbsp;", " ", description)
+      description = re.sub("[\n ]+", " ", description).strip()
+      article.append(description)
     except: continue
     articles.append(article)
 left_col = ['pubDate', 'timestamp', 'title', 'description']
@@ -48,3 +59,4 @@ news = pd.DataFrame(articles, columns=left_col).drop_duplicates().dropna()
 news = news.sort_values(by='timestamp', ascending=False)
 
 news.to_csv('./news/breaking.csv', index=False, quoting=csv.QUOTE_ALL)
+print("\x1b[2K\rSucces : News Line Length : "+str(len(articles)))
--- get_news.py
+++ get_news.py
@@ -6,6 +6,9 @@ import time
 import re
 import csv
 
+from warnings import filterwarnings
+filterwarnings("ignore")
+
 mins = 60 * 24
 articles = []
 rss_list = pd.read_csv('./rss/rss.csv')
@@ -19,18 +22,18 @@ for rss in rss_list['rss-url']:
     soupContent = BeautifulSoup(r.content,features='xml')
     items = soupContent.findAll('item')
     if r.status_code==200 and len(items)!=0:
-      print('Total News Content: ' + str(len(items)) + ' : Success: ' + str(r.status_code) + " : " + rss)
+      print('\x1b[2K\rTotal News Content: ' + str(len(items)) + ' : Success: ' + str(r.status_code) + " : " + rss, end="")
     else:
-      print('Total News Content: 0 : Fail: ' + str(r.status_code) + ' : ' + rss)
+      print('\x1b[2K\rTotal News Content: 0 : Fail: ' + str(r.status_code) + ' : ' + rss, end="")
       continue
   except:
-    print('Total News Content: 0 : Fail: 404 : ' + rss)
+    print('\x1b[2K\rTotal News Content: 0 : Fail: 404 : ' + rss, end="")
     continue
   for i in items:
     article = []
     try:
-      date = re.sub("<.+?>", " ", i.find('pubDate').text).strip()
-      stamp = parse(date, fuzzy=True).timestamp()
+      date = parse(re.sub("<.+?>", " ", i.find('pubDate').text).strip(), fuzzy=True)
+      stamp = date.timestamp()
       if stamp>=time.time() - mins*60:
         article.append(date)
         article.append(stamp)
@@ -38,9 +41,19 @@ for rss in rss_list['rss-url']:
         continue
     except:
        continue
-    try: article.append(re.sub("<.+?>", " ", i.find('title').text).strip())
+    try:
+      title = i.find('title').text
+      title = re.sub("<.+?>", " ", title)
+      title = re.sub("&nbsp;", " ", title)
+      title = re.sub("[\n ]+", " ", title).strip()
+      article.append(title)
     except: continue
-    try: article.append(re.sub("<.+?>", " ", i.find('description').text).strip())
+    try:
+      description = i.find('description').text
+      description = re.sub("<.+?>", " ", description)
+      description = re.sub("&nbsp;", " ", description)
+      description = re.sub("[\n ]+", " ", description).strip()
+      article.append(description)
     except: continue
     try: article.append(i.find('link').text.strip())
     except: continue
@@ -52,3 +65,4 @@ news = news.sort_values(by='timestamp', ascending=False)
 news = pd.merge(news, rss_list, how='inner', on='rss-url')
 
 news.to_csv('./news/news.csv', index=False, quoting=csv.QUOTE_ALL)
+print("\x1b[2K\rSucces : News Line Length : "+str(len(articles)))
