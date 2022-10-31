84586515af1b1d7b17b2987e135e55ab32bfcf02
Sreetam <fearthesreetam@gmail.com>
2022-10-31 13:12:35 +0530

    trmp commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  const fetchData = () => {
+    fetch(`./data/breaking.json`)
+      .then((response) => response.json())
+      .then((actualData) => {
+        console.log(actualData);
+        setData(actualData.products);
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {<tbody>
+        <tr>
+          <th>Date</th>
+          <th>Title</th>
+          <th>Description</th>
+        </tr>
+        {data.map((item) => (
+          <tr>
+            <td>{item.pubDate}</td>
+            <td>{item.title}</td>
+            <td>{item.description}</td>
+          </tr>
+        ))}
+      </tbody>}
     </div>
   );
 }
